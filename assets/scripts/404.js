0e9a7062add463619b696042d24eb5fc9e6c5036
Iker Nieto <93119213+kinire98@users.noreply.github.com>
2023-03-01 20:02:41 +0100

    Update 404.js

--- assets/scripts/404.js
+++ assets/scripts/404.js
@@ -1,5 +1,5 @@
 import { changeLanguageText } from "./modules/changeLanguageText.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    changeLanguageText("h2", "This is no the page you are looking for", "Esta no es la página que estás buscando");
+    changeLanguageText("h2", "This isn't the page you are looking for", "Esta no es la página que estás buscando");
 });
