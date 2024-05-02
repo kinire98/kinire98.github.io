bcb13e1716737f7eab7bcad0c46ba7f0203c1c10
kinire98 <iknitgarri@outlook.com>
2024-05-02 14:18:13 +0200

    Added more things to the projects page

--- src/helpers/openPage.ts
+++ src/helpers/openPage.ts
@@ -1,10 +1,10 @@
+
 export default function openLink($el: HTMLElement, url: string) {
     $el.style.cursor = 'pointer';
     $el.addEventListener('click', () => {
         const $a = document.createElement('a');
         $a.style.display = 'none';
         $a.href = url;
-        $a.target = '_blank';
         document.body.appendChild($a);
         $a.click();
     })
