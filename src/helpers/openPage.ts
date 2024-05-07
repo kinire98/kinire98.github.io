f9f1c003f80f37f101c9f3a0ec3907f6d05dd884
kinire98 <iknitgarri@outlook.com>
2024-05-07 12:39:04 +0200

    Responsive test

--- src/helpers/openPage.ts
+++ src/helpers/openPage.ts
@@ -1,11 +1,11 @@
 
 export default function openLink($el: HTMLElement, url: string) {
     $el.style.cursor = 'pointer';
+    const $a = document.createElement('a');
+    $a.style.display = 'none';
+    $a.href = url;
+    document.body.appendChild($a);
     $el.addEventListener('click', () => {
-        const $a = document.createElement('a');
-        $a.style.display = 'none';
-        $a.href = url;
-        document.body.appendChild($a);
         $a.click();
     })
 }
