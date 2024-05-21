93a174d77ed79827f307dd6501262339a60ca042
kinire98 <iknitgarri@outlook.com>
2024-05-21 12:56:14 +0200

    Apply code formats and added project counter

--- src/helpers/openPage.ts
+++ src/helpers/openPage.ts
@@ -1,11 +1,10 @@
-
 export default function openPage($el: HTMLElement, url: string) {
-    $el.style.cursor = 'pointer';
-    const $a = document.createElement('a');
-    $a.style.display = 'none';
-    $a.href = url;
-    document.body.appendChild($a);
-    $el.addEventListener('click', () => {
-        $a.click();
-    })
+	$el.style.cursor = "pointer";
+	const $a = document.createElement("a");
+	$a.style.display = "none";
+	$a.href = url;
+	document.body.appendChild($a);
+	$el.addEventListener("click", () => {
+		$a.click();
+	});
 }
--- src/helpers/shuffleArray.ts
+++ src/helpers/shuffleArray.ts
@@ -1,16 +1,17 @@
 export default function shuffleArray(array: any[]): any[] {
-    let currentIndex: number = array.length;
+	let currentIndex: number = array.length;
 
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
+	// While there remain elements to shuffle...
+	while (currentIndex != 0) {
+		// Pick a remaining element...
+		let randomIndex: number = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
 
-    // Pick a remaining element...
-    let randomIndex: number = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-  return array;
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex],
+			array[currentIndex],
+		];
+	}
+	return array;
 }
